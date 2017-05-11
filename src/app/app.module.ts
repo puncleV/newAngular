d106f1a7a3388fe748bf5029e43e2ca337c72169
Victor Kozlov <punkrus18@gmail.com>
2017-05-11 11:52:52 +0300

    api sim

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HttpModule }    from '@angular/http';
 
 import { HeroesComponent }  from './heroes.component';
-import {AppComponent} from "./app.component";
-import {DashboardComponent} from "./dashboard.component";
-import {AppRoutingModule} from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./dashboard.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   declarations: [
     AppComponent,
